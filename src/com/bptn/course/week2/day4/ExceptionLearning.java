13bef8c57a51756ae172d5295cd8bdec304082d0
Adetola <adetolafadairo@gmail.com>
2023-08-31 06:53:46 -0700

    completing an example of exception

---
+++ src/com/bptn/course/week2/day4/ExceptionLearning.java
@@ -0,0 +1,20 @@
+package com.bptn.course.week2.day4;
+
+public class ExceptionLearning {
+
+	public static void main(String[] args) {
+
+        try {
+            
+        	int a = 11;
+        	int b = 0;
+        	System.out.println(a / b);
+        	
+        } catch (ArithmeticException e) {
+        	
+        	System.out.println("The number canot be divided by zero");
+            
+        }
+    }
+	
+}
