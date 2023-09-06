c83f9e8f7564ad3baf5e1858c5063c8eb4cac6ce
Adetola <adetolafadairo@gmail.com>
2023-09-06 09:03:10 -0700

    working on hash map manipulation:

---
+++ src/com/bptn/course/week2/day1/four_in_row/InvalidMoveException.java
@@ -0,0 +1,7 @@
+package com.bptn.course.week2.day1.four_in_row;
+
+public class InvalidMoveException extends Exception {
+	public InvalidMoveException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/bptn/course/week3/day3/HashSetManipulation2.java
@@ -0,0 +1,50 @@
+package com.bptn.course.week3.day3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetManipulation2 {
+	
+	public static void main(String[] args) {
+		// Create a HashSet and populate it with initial values
+		Set<String> set = new HashSet<>();
+		
+		set.add("A");
+		set.add("B");
+		set.add("C");
+
+		// Print the size of the set
+		System.out.println("Set size: " + set.size());
+		
+
+		// Use for loop to print the values in the set
+		for (String value: set) {
+			System.out.println("Value: " + value);
+		}
+		
+
+		// Use add() method to add a new value to the set
+		set.add("D");
+		System.out.println("Set after using add():");
+		for (String value: set) {
+			System.out.println("Value: " + value);
+		}
+
+		// Use remove() method to remove a value from the set
+		set.remove("A");
+		System.out.println("Set after using remove():");
+		for (String value: set) {
+			System.out.println("Value: " + value);
+		}
+		
+
+		// Use contains() method to check if the value "C" exists in the set
+		if(set.contains("C")) {
+			System.out.println("Value 'C' exists in the set");
+		} else {
+			System.out.println("Value 'C' does not exist in the set");
+		}
+		
+	}
+
+}
